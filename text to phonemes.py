85ef74a67c5e4f39726c7cbf79e7f69d451f4c04
DomPulse <32404918+DomPulse@users.noreply.github.com>
2018-02-11 12:10:17 -0500

    Create text to phonemes.py

---
+++ text to phonemes.py
@@ -0,0 +1,34 @@
+import nltk #nltk = natural language tool kit
+from nltk.tokenize import word_tokenize
+from nltk.corpus import cmudict #this is how GLaDOS knows how to pronounce words
+pronunciations = cmudict.dict() #this makes the function easier to call
+
+def combine(words):  #This function recombines contractions and takes out commas
+    x = []
+    for w in words:
+        if w != "," and "'" not in w:
+            x.append(w)
+        elif "'" in w:
+            x[len(x)-1] += w
+    
+    return(x)
+
+def process_content():
+    while True:
+        input_text = input("Type a sentance:") #This is the sentance GLaDOS will say
+        input_text = input_text.lower()
+        input_text = input_text.replace(".", " ")
+    
+        try:
+            words = word_tokenize(input_text)
+            words = combine(words)
+            print(words)
+            for w in words:
+                if len(pronunciations[w]) > 1:
+                    print ((pronunciations[w])[0])
+                else:
+                    print(pronunciations[w])    
+        except Exception as e:
+            print (str(e))
+            
+process_content()
